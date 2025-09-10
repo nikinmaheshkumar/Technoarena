9bb71e207af32039ed8b6c418bd7facc686c32dd
Cainvon17 <raghavanmuthuraj@gmial.com>
2025-09-10 11:18:36 +0530

    fixed responsivity and ui update

--- src/pages/AboutEvent.jsx
+++ src/pages/AboutEvent.jsx
@@ -4,7 +4,8 @@ const AboutEvent = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-base-200">
       
-      <h1 className='heading'>TECHNO ARENA</h1>
+<img src="https://fontmeme.com/permalink/250910/e2b66775309d9b3d9cd3efd2f5fdc9a6.png" alt="stranger-things-font" border="0"/>
+      {/* <h1 className='heading'>TECHNO ARENA</h1> */}
       <h2>The Ultimate Battle of Wits, Speed & Survival</h2>
       <h3 className='sep'>Step into Techno Arena, an electrifying competition inspired by the mystery and thrill of Stranger Things.</h3>
       <h2>Seven games But One ultimate winner!</h2>
